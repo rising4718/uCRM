1e3e989bfedb4b28ed31d1c7c1db365e05231251
rise1997 <rise0077>
2024-11-01 17:35:36 +0900

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!-- resources/views/test.blade.php -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>日本語テスト</title>
+</head>
+<body>
+    <h1>{{ __('messages.welcome') }}</h1>
+</body>
+</html>
